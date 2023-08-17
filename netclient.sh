ea7ad7a12913959f27f80da1cfdd689ae27d831f
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-08-17 02:40:16 -0300

    Create netclient.sh

---
+++ netclient.sh
@@ -0,0 +1,5 @@
+#Made by Netmaker
+curl -sL 'https://apt.netmaker.org/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/netclient.asc
+curl -sL 'https://apt.netmaker.org/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/netclient.list
+sudo apt update
+sudo apt install netclient
