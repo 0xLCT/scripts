0c9147c150fd27fbb4481f5eeb93a2cd2335f701
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-08-17 01:23:00 -0300

    Create syncthing.sh

---
+++ syncthing.sh
@@ -0,0 +1,8 @@
+CURRENT_USER=$(id -un)
+
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+sudo apt update
+sudo apt install syncthing
+sudo systemctl enable "syncthing@$CURRENT_USER.service"
+sudo systemctl start "syncthing@$CURRENT_USER.service"
