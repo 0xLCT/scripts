f0dc8a2b298191d8f3a9c76e60d455885a1c27c6
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-08-17 22:49:39 -0300

    Update netclient.sh

--- netclient.sh
+++ netclient.sh
@@ -1,5 +1,6 @@
 #Made by Netmaker
+sudo apt install wireguard #Install wireguard first to avoid issues
 curl -sL 'https://apt.netmaker.org/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/netclient.asc
 curl -sL 'https://apt.netmaker.org/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/netclient.list
 sudo apt update
-sudo apt install netclient wireguard
+sudo apt install netclient 
