948e64decc6942b282244fc74f4755a4cc2b3e8c
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-09-14 14:15:22 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
-    gnupg
+    gnupg -y
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
