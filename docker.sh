5714fef5bcbc0882f558b1df6a62cf1b7495e124
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-10-05 21:11:38 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -19,7 +19,7 @@ install_dependencies() {
 }
 
 # Function to add Docker's GPG key
-add_docker_gpg() {
+add_docker_gpg_debian() {
     local url=\$1
     sudo mkdir -m 0755 -p /etc/apt/keyrings
     curl -fsSL $url | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -28,6 +28,16 @@ add_docker_gpg() {
         exit 1
     fi
 }
+add_docker_gpg_ubuntu() {
+    local url=\$1
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    if [ $? -ne 0 ]; then
+        echo "Failed to add Docker's GPG key"
+        exit 1
+    fi
+}
+
 
 # Check if system is Debian or Ubuntu and install Docker
 if [ -e /etc/debian_version ]; then
@@ -37,14 +47,14 @@ if [ -e /etc/debian_version ]; then
         sudo apt-get remove docker docker.io containerd runc
         update_packages
         install_dependencies
-        add_docker_gpg "https://download.docker.com/linux/ubuntu/gpg"
+        add_docker_gpg_ubuntu
     # Else must be Debian
     else
         # Remove any existing Docker packages
         for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
         update_packages
         install_dependencies
-        add_docker_gpg "https://download.docker.com/linux/debian/gpg"
+        add_docker_gpg_debian
     fi
     # Add Docker repository 
     echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
