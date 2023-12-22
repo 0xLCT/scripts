61c0bba30b0334bec1c70b677828095ca78fbf53
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-12-22 18:57:28 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,8 +2,14 @@
 
 if grep -q '^ID=ubuntu' /etc/os-release; then
    bash <(curl -s https://raw.githubusercontent.com/lctrevizan/ubuntuscript/main/dockerubuntu.sh)
+   sudo groupadd docker
+   sudo usermod -aG docker $USER
+   newgrp docke
 elif grep -q '^ID=debian' /etc/os-release; then
    bash <(curl -s https://raw.githubusercontent.com/lctrevizan/ubuntuscript/main/dockerdeb.sh)
+   sudo groupadd docker
+   sudo usermod -aG docker $USER
+   newgrp docker
 else
    echo "Unknown OS"
 fi
