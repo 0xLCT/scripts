2c54e3f56e3f8e37dc95cd05e989371dfd450768
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2023-12-22 18:57:40 -0300

    Update docker.sh
    Typo

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ if grep -q '^ID=ubuntu' /etc/os-release; then
    bash <(curl -s https://raw.githubusercontent.com/lctrevizan/ubuntuscript/main/dockerubuntu.sh)
    sudo groupadd docker
    sudo usermod -aG docker $USER
-   newgrp docke
+   newgrp docker
 elif grep -q '^ID=debian' /etc/os-release; then
    bash <(curl -s https://raw.githubusercontent.com/lctrevizan/ubuntuscript/main/dockerdeb.sh)
    sudo groupadd docker
