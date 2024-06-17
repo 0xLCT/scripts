89b0829b2de4fc1bbd5fc7483a5e0ac949939c95
lctrevizan <106083845+lctrevizan@users.noreply.github.com>
2024-06-16 22:33:36 -0300

    Update setup.sh

--- deb/setup.sh
+++ deb/setup.sh
@@ -12,9 +12,6 @@ sudo apt update
 sudo apt upgrade -y
 
 # Add Git PPA and install latest Git
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
-sudo apt update
 sudo apt install git -y
 
 # Add user 'lucas'
