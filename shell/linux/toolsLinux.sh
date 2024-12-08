28abcccda47b2d01ab0ad880512a5b0fa179a757
0xLCT <l@lct.cx>
2024-12-08 18:08:10 -0300

    Fixed issues caused by moving

--- shell/linux/toolsLinux.sh
+++ shell/linux/toolsLinux.sh
@@ -6,32 +6,24 @@ main() {
     }
     function1() {
         echo "Debian"
-<<<<<<< HEAD:shell/linux/toolsLinux.sh
-        curl -fsS https://raw.githubusercontent.com/0xLCT/scripts/main/shell/linux/deb/toolsDeb.sh
+        bash <(curl -s https://raw.githubusercontent.com/0xLCT/scripts/master/shell/linux/deb/toolsDeb.sh)
     }
     function2() {
         echo "Redhat"
-        curl -fsS https://raw.githubusercontent.com/0xLCT/scripts/main/shell/linux/rh/toolsRh.sh
-=======
-        bash <(curl -fsS https://raw.githubusercontent.com/lctrevizan/debscripts/main/linux/deb/toolsDeb.sh)
+        bash <(curl -s https://raw.githubusercontent.com/0xLCT/scripts/master/shell/linux/rh/toolsRh.sh)
     }
-    function2() {
-        echo "Redhat"
-        bash <(curl -fsS https://raw.githubusercontent.com/lctrevizan/debscripts/main/linux/rh/toolsRh.sh)
->>>>>>> 52e2e8c7560a82f8c23643e78fb2c920c8894326:linux/toolsLinux.sh
+    function5() {
+        echo "Setup zsh"
+        bash <(curl -s https://raw.githubusercontent.com/0xLCT/scripts/master/shell/linux/zshsetup.sh)
     }
 
     function9() {
         echo "[9] Flatpaks choosen"
-<<<<<<< HEAD:shell/linux/toolsLinux.sh
-        curl -fsS https://raw.githubusercontent.com/0xLCT/scripts/main/shell/linux/flatpaks.sh | sh
-=======
-        bash <(curl -fsS https://raw.githubusercontent.com/lctrevizan/debscripts/main/linux/flatpaks.sh)
->>>>>>> 52e2e8c7560a82f8c23643e78fb2c920c8894326:linux/toolsLinux.sh
+        bash <(curl -s https://raw.githubusercontent.com/0xLCT/scripts/master/shell/linux/flatpaks.sh)
     }
 
 
-    printf "Linux Tools\n\nSelect an option 0-9 \n\n[1] Debian\n[2] Redhat\n[9] Install flatpaks\n[0] Exit\n "
+    printf "Linux Tools\n\nSelect an option 0-9 \n\n[1] Debian\n[2] Redhat\n[5] Setup ZSH\n[9] Install flatpaks\n[0] Exit\n "
     read num
 
     if [ "$num" -ge 0 ] && [ "$num" -le 9 ]; then
